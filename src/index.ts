eef5d84fa2c533ea2730150c683da2ec4ed542b5
Bulkan Evcimen <bulkan@gmail.com>
2020-07-28 21:56:46 +1000

    Inksplatter

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,23 @@
 import p5 from "p5";
 import palettes from 'nice-color-palettes';
 
-
 const makeArt = (p: p5) => {
+  let points;
+  const count = 30;
+  const margin = 120;
   const palette = palettes[Math.floor(p.random() * palettes.length - 1)];
+  // const color = p.color(palette[Math.floor(p.random() * palette.length)]);
+  const color = p.color('black');
  
   const createGrid = (count) => {
     const points = [];
 
-    for (let x = 0; x < count; x++) {
-      const color = p.color(palette[Math.floor(p.random() * palette.length)]);
-      color.setAlpha(p.map(p.noise(x), 0, 1, 0, 255));
+    for (let x = 0; x <= count; x++) {      
+      for (let y = 0; y <= count; y++) {
+        const u = x / (count - 1);
+        const v = y / (count - 1);
 
-      for (let y = 0; y < count; y++) {
-        const u = count <= 1 ? 0.5 : x / (count - 1);
-        const v = count <= 1 ? 0.5 : y / (count - 1);
-
-        const radius = p.map(p.noise(u, v), 0, 1, 0, p.width * 0.09);
+        const radius = p.map(p.noise(u, v), 0, 1, 10, 100);
         const rotation = p.map(p.noise(u, v), 0, 1, 0, p.PI);
 
         points.push({
@@ -31,26 +32,33 @@ const makeArt = (p: p5) => {
     return points;
   };
 
-  let points; 
+  const rect = (x, y, width, color) => {
+    const MAX_POINTS = p.randomGaussian(200, 1000);
 
-  const count = 30;
-  const margin = 100;
+    color.setAlpha(p.map(MAX_POINTS, 200, 1000, 255, 1));
+    p.fill(color);
 
-  let zoff = 0.0;
+    for (let i = 0; i < MAX_POINTS; i++) {
+      const noise = p.noise(i);
+      const d = p.map(noise, 0, 1, 0, 3);
+
+      const x1 = p.random(x, x + width);
+      const y1 = p.random(y, y + width);
+
+      p.circle(x1 + noise * 2, y1 + noise * 2, d);
+    }
+  }
 
   p.setup = () => {
-    p.createCanvas(800, 800);
+    p.createCanvas(900, 900);
     p.background("white");
     p.noLoop();
     p.noStroke();
 
-    points = createGrid(count).filter(() => p.randomGaussian() > 0.5);
+    points = createGrid(count).filter(() => p.randomGaussian() > 0.35);
   };
 
   p.draw = () => {
-    // p.fill('black')
-    // p.textFont('Helvetica');
-
     const { width, height } = p;
     
     points.forEach((data) => {
@@ -59,28 +67,15 @@ const makeArt = (p: p5) => {
       const x = p.lerp(margin, width - margin, position.u);
       const y = p.lerp(margin, height - margin, position.v);
 
-      // p.strokeWeight(1);
-      // p.stroke('black');
-      
-      // p.arc(x, y, radius, radius, 0, Math.PI * 2);
-
-      // data.prevRadius = radius
-      // data.radius = p.map(p.noise(x, y, zoff), 0, 1, 1, 20);
-
       p.push();
-      p.fill(color);
+
       p.translate(x, y);
       p.rotate(rotation);
       
-      p.rect(0, 0, radius, radius);
-
-      // p.text("|", 0, 0);
-
+      // p.rect(0, 0, radius, radius);
+      rect(0, 0, radius, color);
       p.pop();
     });
-    
-    // zoff += 0.006;
-
   };
 };
 
