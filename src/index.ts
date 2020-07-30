0310483dc5f7a867d63816857ca904a8f7e86c85
Bulkan Evcimen <bulkan@gmail.com>
2020-07-31 00:10:46 +1000

    Make the canvas a square in the window

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,46 @@
 import p5 from "p5";
-// import palettes from 'nice-color-palettes';
+import palettes from 'nice-color-palettes';
+
+function fitSquares(width: number, height: number, n): number {
+  let sx = 0
+  let sy = 0;
+
+  const px = Math.ceil(Math.sqrt((n * width) / height));
+
+  if (Math.floor((px * height) / width) * px < n) {
+    sx = height / Math.ceil((px * height) / width);
+  } else {
+    sx = width / px;
+  }
+
+  const py = Math.ceil(Math.sqrt((n * height) / width));
+
+  if (Math.floor((py * width) / height) * py < n) {
+    sy = width / Math.ceil((width * py) / height);
+  } else {
+    sy = height / py;
+  }
+
+  return Math.max(sx, sy);
+}
 
 const makeArt = (p: p5) => {
   let points;
 
   const count = 50;
 
+  const MIN_WIDTH = 5;
+  const MAX_WIDTH = 90;
+
   const loading = p.select('#loading');
 
-  // const palette = palettes[Math.floor(p.random() * palettes.length - 1)];
+  const palette = palettes[Math.floor(p.random() * palettes.length - 1)];
   // const color = p.color(palette[Math.floor(p.random() * palette.length)]);
   const color = p.color('black');
  
   const createGrid = (count) => {
     const points = [];
 
-    const MIN_WIDTH = 5;
-    // const MAX_WIDTH = 70;
-    const MAX_WIDTH = count * 2;
-  
     for (let x = 0; x < count; x++) {      
       for (let y = 0; y < count; y++) {
         const u = x / (count - 1);
@@ -39,37 +61,43 @@ const makeArt = (p: p5) => {
     return points;
   };
 
-  const rect = (x, y, width, color) => {
-    const MAX_POINTS = p.randomGaussian() * width * 40;
+  const rect = (x, y, width, color: p5.Color) => {
+    // const MAX_POINTS = p.randomGaussian() * 1000;
+    const MAX_POINTS = p.map(width, MIN_WIDTH, MAX_WIDTH, count, 4000);
 
-    color.setAlpha(p.map(MAX_POINTS, 0, width * 40, 120, 1));
+    color.setAlpha(p.map(MAX_POINTS, count, 4000, 190, 1));
     p.fill(color);
 
     for (let i = 0; i < MAX_POINTS; i++) {
-      const noise = p.noise(i);
-      // const d = p.map(noise, 0, 1, 0, 3);
-      const d = 2;
+      const noise = p.noise(i, MAX_POINTS * 0.1) * 4;
 
-      const x1 = p.random(x, x + width);
-      const y1 = p.random(y, y + width);
+      const x1 = p.random(x + noise, x + width + noise);
+      const y1 = p.random(y + noise, y + width + noise );
 
-      p.circle(x1 + noise * 2, y1 + noise * 2, d);
+      p.circle(x1, y1, 1);
     }
   }
 
   p.setup = () => {
-    const seed = p.random() * 100; 
+    const seed =  p.random() * 1000; 
+
+    // interesting seeds
+      // 74.0410697292676;
+      // 42.545741789881994
     
     p.randomSeed(seed);
+    p.noiseSeed(seed);
     
     console.table({ seed });
+
+    const width = fitSquares(p.windowWidth - 10, p.windowHeight - 10, 1);
     
-    p.createCanvas(p.windowWidth * .55, p.windowWidth * .55);
+    p.createCanvas(width, width);
     p.background("white");
     p.noLoop();
     p.noStroke();
     
-    points = createGrid(count).filter(() => p.randomGaussian() > 0.35);
+    points = createGrid(count).filter(() => p.randomGaussian() > 0.55);
     // points = createGrid(count);
   };
   
