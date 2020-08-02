e495605ca218801e74d46f1c579c427ac39ed105
Bulkan Evcimen <bulkan@gmail.com>
2020-08-02 19:42:31 +1000

    Use Buefy

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import Vue from 'vue';
+import Buefy from 'buefy'
 import App from './components/App.vue';
+import 'buefy/dist/buefy.css'
+
+Vue.use(Buefy);
 
 new Vue({ render: createElement => createElement(App) }).$mount('#app');
