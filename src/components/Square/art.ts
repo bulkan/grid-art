22d55022556fd46ebecb227705c078f16fed338c
Bulkan Evcimen <bulkan@gmail.com>
2020-08-04 20:26:01 +1000

    Make it art
    Add responsive meta tag

--- src/components/Square/art.ts
+++ src/components/Square/art.ts
@@ -1,23 +1,40 @@
 import p5 from "p5";
 import palettes from 'nice-color-palettes/200.json';
-import random from 'canvas-sketch-util/random';
 import { halton as Halton } from 'low-discrepancy-sequence';
 
 const sketch = (p: p5) => {
   let points;
-  const count = 21;
-  const MAX_POINTS = 5000;
-  const MIN_POINTS = MAX_POINTS / 10;
+  let pg;
+
+  const count = 51;
+  const MAX_POINTS = 2000;
+  const MIN_POINTS = 1000;
   const MIN_WIDTH = 21;
-  const MAX_WIDTH = 105;
-
+  const MAX_WIDTH = 84;
+  const CANVAS_WIDTH = 600;
   const haltonSequence = new Halton();
+  const seed = Math.floor(Math.random() * 1000000);
 
+  // interesting seeds
+    // 101435
+    // 699087
+    // 15965086346200000
+    // 527.5082494902459
+    // 74.0410697292676;
+    // 42.545741789881994
+  
+  p.randomSeed(seed);
+  p.noiseSeed(seed);
+
+  // Interesting palettes = 62, 108
   const paletteIndex = Math.floor(p.random() * palettes.length - 1);
   const palette = p.shuffle(palettes[paletteIndex]);
   const backgroundColor = p.color(palette.pop());
+
+  palette.pop();
   
   const createGrid = (count) => {
+    console.time('createGrid');
     const points = [];
     
     for (let x = 0; x < count; x++) {      
@@ -30,7 +47,7 @@ const sketch = (p: p5) => {
         const width = Math.abs(p.random(MIN_WIDTH, MAX_WIDTH)); 
         const rotation = p.map(p.noise(u, v), 0, 1, 0, p.TWO_PI);
 
-        if((p.random()) > 0.2) {
+        if((p.random()) > 0.55) {
           points.push({
             color,
             rotation,
@@ -41,14 +58,16 @@ const sketch = (p: p5) => {
       }
     }
 
+    console.timeEnd('createGrid');
     return points;
   };
 
-  const rect = (width: number, color: p5.Color) => {
+  const rect = (width: number, color: p5.Color, pg: p5.Graphics) => {
     const POINTS = p.map(width, MIN_WIDTH, MAX_WIDTH, MIN_POINTS, MAX_POINTS);
 
-    color.setAlpha(p.map(POINTS, MIN_POINTS, MAX_POINTS, 150, 100));
-    p.fill(color);
+    const alpha = p.map(POINTS, MIN_POINTS, MAX_POINTS, 175, 75);
+    color.setAlpha(alpha);
+    pg.fill(color);
     
     for (let i = 0; i < POINTS; i++) {
       const [x, y] = haltonSequence.getNext();
@@ -56,7 +75,7 @@ const sketch = (p: p5) => {
       const x1 = x * width;
       const y1 = y * width;
 
-      p.circle(x1, y1, 1);
+      pg.circle(x1, y1, 1);
     }
   }
 
@@ -67,39 +86,32 @@ const sketch = (p: p5) => {
         return false;
       }
     };
-
-    const seed =  random.getRandomSeed();
     
-    // interesting seeds
-    // 527.5082494902459
-    // 74.0410697292676;
-    // 42.545741789881994
-    
-    random.setSeed(seed);
-    p.randomSeed(seed);
-    p.noiseSeed(seed);
+    p.createCanvas(CANVAS_WIDTH, CANVAS_WIDTH);
+    pg = p.createGraphics(CANVAS_WIDTH, CANVAS_WIDTH)
 
-    const width = 750;
-
+    backgroundColor.setAlpha(128);
+    p.background(backgroundColor);
+    p.rectMode(p.CENTER);
     p.noLoop();
     p.noStroke();
-    p.createCanvas(width, width);
-    backgroundColor.setAlpha(100);
-    p.background(backgroundColor);
-    
-    // points = createGrid(count).filter(() => ;
+
+    pg.background(backgroundColor);
+    pg.noStroke();
+    pg.rectMode(p.CENTER);
+
     points = createGrid(count);
         
     console.table({ 
       seed,
       palette,
       paletteIndex,
-      width,
       pointsLegth: points.length,
       backgroundColor: backgroundColor.toString(),
       MAX_POINTS,
       MAX_WIDTH,
-      MIN_WIDTH
+      MIN_WIDTH,
+      CANVAS_WIDTH
     });
 
   };
@@ -107,24 +119,24 @@ const sketch = (p: p5) => {
   p.draw = () => {
     console.time('draw');
     const margin = p.width * 0.075 - MAX_WIDTH;
-    p.rectMode(p.CENTER);
+
     points.forEach(data => {
       
       const { width, color, position, rotation = 0 } = data;
       
-      const x = p.lerp(margin, p.width - margin, position.u);
-      const y = p.lerp(margin, p.height - margin, position.v);
+      const x = p.abs(p.lerp(margin, p.width - margin, position.u));
+      const y = p.abs(p.lerp(margin, p.height - margin, position.v));
+
+      pg.translate(x, y);
+      pg.rotate(rotation);
       
-      p.push();
+      rect(width, color, pg);
       
-      p.translate(x, y);
-      p.rotate(rotation);      
-      rect(width, color);
-      // p.stroke(color);
-      // p.rect(0, 0, 50, 50);
-      
-      p.pop();
+      pg.resetMatrix();
+      p.image(pg, 0, 0, CANVAS_WIDTH, CANVAS_WIDTH)
     });
+
+    p.image(pg, 0, 0, CANVAS_WIDTH, CANVAS_WIDTH)
     console.timeEnd('draw');
   };
 };
