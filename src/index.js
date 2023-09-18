cedd1a8ea350c73d7eaca0f03f7c340412ff7790
Bulkan Evcimen <bulkan@gmail.com>
2023-09-18 21:04:36 +1000

    Upgrade buefy

--- src/index.js
+++ src/index.js
@@ -1,30 +1,31 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router'
+import Vue from "vue";
+import VueRouter from "vue-router";
 // import Buefy from 'buefy'
-import App from './components/App.vue';
-import Home from './components/Home.vue';
-import Art from './components/Art.vue';
-import Square, { name as squareRoute } from './components/Square/Square.vue';
-import Lines, { name as linesRoute} from './components/Lines/Lines.vue';
-import Squiggles, { name as squigglesRoute} from './components/Squiggles/Squiggles.vue';
-import 'buefy/dist/buefy.css'
-
+import App from "./components/App.vue";
+import Home from "./components/Home.vue";
+import Art from "./components/Art.vue";
+import Square, { name as squareRoute } from "./components/Square/Square.vue";
+import Lines, { name as linesRoute } from "./components/Lines/Lines.vue";
+import Squiggles, {
+  name as squigglesRoute
+} from "./components/Squiggles/Squiggles.vue";
+import "buefy/dist/buefy.css";
 
 const routes = [
-  { name: squareRoute, path: `/${squareRoute}`, component: Square},
-  { name: linesRoute, path: `/${linesRoute}`, component: Lines},
-  { name: squigglesRoute, path: `/${squigglesRoute}`, component: Squiggles},
-  { name: 'home', path: '/', component: Home },
+  { name: squareRoute, path: `/${squareRoute}`, component: Square },
+  { name: linesRoute, path: `/${linesRoute}`, component: Lines },
+  { name: squigglesRoute, path: `/${squigglesRoute}`, component: Squiggles },
+  { name: "home", path: "/", component: Home }
 ];
 
 // Vue.use(Buefy);
 Vue.use(VueRouter);
 
-Vue.component('Art', Art);
+Vue.component("Art", Art);
 
-const router = new VueRouter({  mode: 'history', routes });
+const router = new VueRouter({ mode: "history", routes });
 
 new Vue({
   router,
-  render: createElement => createElement(App) 
-}).$mount('#app');
+  render: createElement => createElement(App)
+}).$mount("#app");
