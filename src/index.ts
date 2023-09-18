a176627b766556a32a93e55ff426541a9ac5f919
Bulkan Evcimen <bulkan@gmail.com>
2023-09-18 21:34:25 +1000

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,30 @@
-import Vue from "vue";
+import { createApp } from "vue";
 import VueRouter from "vue-router";
 // import Buefy from 'buefy'
-import App from "./src/components/App.vue";
-import Home from "./src/components/Home.vue";
-import Art from "./src/components/Art.vue";
-import Square, {
-  name as squareRoute
-} from "./src/components/Square/Square.vue";
-import Lines, { name as linesRoute } from "./src/components/Lines/Lines.vue";
-import Squiggles, {
-  name as squigglesRoute
-} from "./components/Squiggles/Squiggles.vue";
+import App from "./components/App.vue";
+import Home from "./components/Home.vue";
+// import Art from "./components/Art.vue";
+// import Square, { name as squareRoute } from "./components/Square/Square.vue";
+// import Lines, { name as linesRoute } from "./components/Lines/Lines.vue";
+// import Squiggles, {
+//   name as squigglesRoute
+// } from "./components/Squiggles/Squiggles.vue";
 import "buefy/dist/buefy.css";
 
 const routes = [
-  { name: squareRoute, path: `/${squareRoute}`, component: Square },
-  { name: linesRoute, path: `/${linesRoute}`, component: Lines },
-  { name: squigglesRoute, path: `/${squigglesRoute}`, component: Squiggles },
+  // { name: squareRoute, path: `/${squareRoute}`, component: Square },
+  // { name: linesRoute, path: `/${linesRoute}`, component: Lines },
+  // { name: squigglesRoute, path: `/${squigglesRoute}`, component: Squiggles },
   { name: "home", path: "/", component: Home }
 ];
 
-// Vue.use(Buefy);
-Vue.use(VueRouter);
+const router = VueRouter.createRouter({
+  // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+  history: VueRouter.createWebHashHistory(),
+  routes // short for `routes: routes`
+});
 
-Vue.component("Art", Art);
+const app = createApp(App);
 
-const router = new VueRouter({ mode: "history", routes });
-
-new Vue({
-  router,
-  render: createElement => createElement(App)
-}).$mount("#app");
+app.use(router);
+app.mount("#app");
